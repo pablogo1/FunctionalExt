1e8d3dd30108da202f3b4ce3894cb4db9d0e4c76
Pablo Gonzalez <3784129+pablogo1@users.noreply.github.com>
2024-05-28 08:26:36 -0600

    Added Success helper function.
    Signed-off-by: Pablo Gonzalez <3784129+pablogo1@users.noreply.github.com>

---
+++ src/FunctionalExt/Result/Functions.cs
@@ -0,0 +1,7 @@
+﻿namespace FunctionalExt;
+
+public static partial class Functions
+{
+    public static Result<T, Error> Success<T>(T value) => Success<T, Error>(value);
+    public static Result<T, TError> Success<T, TError>(T value) where TError : IError => Result<T, TError>.Create(value);
+}
---
+++ tests/FunctionalExt.Tests/Result/Functions/SuccessTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FunctionalExt.Tests.Result;
+
+using static Functions;
+
+public class SuccessTests
+{
+    [Fact]
+    public void Should_create_successful_result_with_value()
+    {
+        var result = Success("test");
+
+        result.Should().BeOfType<Result<string, Error>>();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be("test");
+    }
+}
