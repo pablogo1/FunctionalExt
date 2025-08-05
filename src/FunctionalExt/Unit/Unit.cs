db24568ad74e529dd5dfb338a9e01b2a20a75809
Pablo Gonzalez <3784129+pablogo1@users.noreply.github.com>
2025-08-04 23:49:33 -0600

    u Unit added Task static property.

--- src/FunctionalExt/Unit/Unit.cs
+++ src/FunctionalExt/Unit/Unit.cs
@@ -1,6 +1,13 @@
 namespace FunctionalExt;
 
+/// <summary>
+/// Represents a type with a single value, used to indicate the absence of a specific value or as a return type for operations that do not return a result.
+/// </summary>
+/// <remarks>
+/// Commonly used in functional programming to represent a void type in generic contexts.
+/// </remarks>
 public readonly record struct Unit
 {
     public static Unit Default { get; } = new();
+    public static Task<Unit> Task { get; } = System.Threading.Tasks.Task.FromResult(Default);
 }
